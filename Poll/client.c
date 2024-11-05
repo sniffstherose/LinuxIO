1f029e59b16cdd1de50754a2211a6a300a8269bb
sniffstherose <3383876329@qq.com>
2024-11-05 10:29:55 +0800

    Changes

--- Poll/client.c
+++ Poll/client.c
@@ -1,3 +1,52 @@
-//
-// Created by cat on 2024/11/5.
-//
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define SERVERIP "127.0.0.1"
+#define PORT 6789
+
+int main()
+{
+    // 1. 创建socket（用于通信的套接字）
+    int connfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (connfd == -1) {
+        perror("socket");
+        exit(-1);
+    }
+    // 2. 连接服务器端
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = PF_INET;
+    inet_pton(AF_INET, SERVERIP, &server_addr.sin_addr.s_addr);
+    server_addr.sin_port = htons(PORT);
+    int ret = connect(connfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (ret == -1) {
+        perror("connect");
+        exit(-1);
+    }
+    // 3. 通信
+    char recv_buf[1024] = {0};
+    while (1) {
+        // 发送数据
+        char *send_buf = "client message";
+        write(connfd, send_buf, strlen(send_buf));
+        printf("message send done.\n");
+        // 接收数据
+        ret = read(connfd, recv_buf, sizeof(recv_buf));
+        if (ret == -1) {
+            perror("read");
+            exit(-1);
+        } else if (ret > 0) {
+            printf("recv server data : %s\n", recv_buf);
+        } else {
+            // 表示客户端断开连接
+            printf("client closed...\n");
+        }
+        // 休眠的目的是为了更好的观察，放在此处可以解决read: Connection reset by peer问题
+        sleep(1);
+    }
+    // 关闭连接
+    close(connfd);
+    return 0;
+}
--- Poll/poll.c
+++ Poll/poll.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <poll.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define SERVERIP "127.0.0.1"
-#define PORT "6789"
+#define PORT 6789
 
 int main() {
     // 1. 创建socket套接字，用于监听
@@ -12,4 +16,83 @@ int main() {
         perror("socket");
         exit(EXIT_FAILURE);
     }
+
+    // 2. 绑定地址
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, SERVERIP, &server_addr.sin_addr.s_addr);
+    server_addr.sin_port = htons(PORT);
+    int ret = bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (ret == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    // 3. 监听
+    ret = listen(listenfd, 8);
+    if (ret == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    struct pollfd fds[1024];
+    for (int i = 0; i < 1024; ++i) {
+        fds[i].fd = -1;
+        fds[i].events = POLLIN;
+    }
+    fds[0].fd = listenfd;
+    int nfds = 0;
+    while (1) {
+        // 使用poll，永久阻塞
+        int num = poll(fds, nfds + 1, -1);
+        if (num == -1) {
+            perror("poll");
+            exit(EXIT_FAILURE);
+        } else if (num == 0) {
+            continue;
+        } else {
+            // 判断是否有客户端连接
+            if (fds[0].revents & POLLIN) {
+                struct sockaddr_in client_addr;
+                socklen_t client_addr_len = sizeof(client_addr);
+                int connfd = accept(listenfd, (struct sockaddr*)&client_addr, &client_addr_len);
+                if (connfd == -1) {
+                    perror("accept");
+                    exit(EXIT_FAILURE);
+                }
+                char client_ip[16] = {0};
+                inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_ip, sizeof(client_ip));
+                unsigned short client_port = ntohs(client_addr.sin_port);
+                printf("ip: %s, port: %d\n", client_ip, client_port);
+                for (int i = 1; i < 1024; ++i) {
+                    if (fds[i].fd == -1) {
+                        fds[i].fd = connfd;
+                        fds[i].events = POLLIN;
+                        break;
+                    }
+                }
+                nfds = nfds > connfd ? nfds : connfd;
+            }
+            char recvbuf[1024] = {0};
+            for (int i = 1; i <= nfds; ++i) {
+                if (fds[i].fd != -1 && fds[i].revents & POLLIN) {
+                    ret = read(fds[i].fd, recvbuf, sizeof(recvbuf));
+                    if (ret == -1) {
+                        perror("read");
+                        exit(EXIT_FAILURE);
+                    } else if (ret > 0) {
+                        printf("recv client data: %s\n", recvbuf);
+                        write(fds[i].fd, recvbuf, strlen(recvbuf));
+                    } else {
+                        printf("client closed...\n");
+                        close(fds[i].fd);
+                        fds[i].fd = -1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    close(listenfd);
+    return 0;
 }
