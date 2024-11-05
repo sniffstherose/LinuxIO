c23c60caa418cb421b137100e1facd4a18f649b4
sniffstherose <3383876329@qq.com>
2024-11-05 16:02:18 +0800

    Changes

--- Epoll/client.c
+++ Epoll/client.c
@@ -1,52 +1,56 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
 #define SERVERIP "127.0.0.1"
 #define PORT 6789
 
 int main()
 {
-    // 1. 创建socket（用于通信的套接字）
+    //1. 创建连接套接字
     int connfd = socket(AF_INET, SOCK_STREAM, 0);
     if (connfd == -1) {
         perror("socket");
         exit(-1);
     }
-    // 2. 连接服务器端
+
+    //2. 链接服务器
     struct sockaddr_in server_addr;
-    server_addr.sin_family = PF_INET;
-    inet_pton(AF_INET, SERVERIP, &server_addr.sin_addr.s_addr);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVERIP);
     server_addr.sin_port = htons(PORT);
-    int ret = connect(connfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    int ret = connect(connfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if (ret == -1) {
         perror("connect");
         exit(-1);
     }
-    // 3. 通信
-    char recv_buf[1024] = {0};
+
+    //通信
+    int num = 0;
     while (1) {
-        // 发送数据
-        fgets(recv_buf, sizeof(recv_buf), stdin);
-        write(connfd, recv_buf, strlen(recv_buf));
-        memset(recv_buf, 0, sizeof(recv_buf));
-        // 接收数据
-        ret = read(connfd, recv_buf, sizeof(recv_buf));
-        if (ret == -1) {
+        char sendbuf[1024] = {0};
+        // sprintf(sendbuf, "send data %d\n", num++);
+        fgets(sendbuf, sizeof(sendbuf), stdin);
+        write(connfd, sendbuf, strlen(sendbuf) + 1);
+        memset(sendbuf, 0, sizeof(sendbuf));
+        ret = read(connfd, sendbuf, sizeof(sendbuf));
+        if (ret > 0) {
+            printf("接收到数据: %s\n", sendbuf);
+        }
+        else if (ret == 0) {
+            printf("服务器断开连接\n");
+            break;
+        }
+        else {
             perror("read");
             exit(-1);
-        } else if (ret > 0) {
-            printf("recv server data : %s\n", recv_buf);
-        } else {
-            // 表示客户端断开连接
-            printf("client closed...\n");
         }
-        // 休眠的目的是为了更好的观察，放在此处可以解决read: Connection reset by peer问题
         sleep(1);
     }
-    // 关闭连接
+
     close(connfd);
     return 0;
 }
--- Epoll/epoll_et.c
+++ Epoll/epoll_et.c
@@ -1,105 +1,91 @@
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <sys/epoll.h>
-#include <fcntl.h>
+#include <errno.h>
 
-#define SERVERIP "127.0.0.1"
-#define PORT 6789
+int main()
+{
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-int main() {
-    // 1. 创建监听套接字
-    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listenfd == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    // 2. 绑定
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    inet_pton(AF_INET, SERVERIP, &server_addr.sin_addr.s_addr);
-    server_addr.sin_port = htons(PORT);
-    int ret = bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if (ret == -1) {
-        perror("bind");
-        exit(EXIT_FAILURE);
-    }
-    // 3. 监听
-    ret = listen(listenfd, 8);
-    if (ret == -1) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    // 创建epoll实例
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(6789);
+    bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    listen(listen_fd, 8);
+
     int epfd = epoll_create(100);
-    // 监听文件描述符加入实例
     struct epoll_event event;
     event.events = EPOLLIN;
-    event.data.fd = listenfd;
-    ret = epoll_ctl(epfd, EPOLL_CTL_ADD, listenfd, &event);
+    event.data.fd = listen_fd;
+    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, listen_fd, &event);
     if (ret == -1) {
         perror("epoll_ctl");
-        exit(EXIT_FAILURE);
+        exit(-1);
     }
-    // 用来保存返回的信息
+
     struct epoll_event events[1024];
     while (1) {
-        // 使用epoll_wait，并设置永久阻塞
-        int nums = epoll_wait(epfd, events, 1024, -1);
-        if (nums == -1) {
+        int num = epoll_wait(epfd, events, 1024, -1);
+        if (num == -1) {
             perror("epoll_wait");
-            exit(EXIT_FAILURE);
-        } else if (nums == 0) {
-            continue;
-        } else {
-            for (int i = 0; i < nums; ++i) {
-                int curfd = events[i].data.fd;
-                // 有新的连接
-                if (curfd == listenfd) {
-                    struct sockaddr_in client_addr;
-                    socklen_t len = sizeof(client_addr);
-                    int connfd = accept(listenfd, (struct sockaddr*)&client_addr, &len);
-                    if (connfd == -1) {
-                        perror("accept");
-                        exit(EXIT_FAILURE);
+            exit(-1);
+        }
+        for (int i = 0; i < num; ++i) {
+            int curfd = events[i].data.fd;
+            if (curfd == listen_fd) {
+                struct sockaddr_in client_addr;
+                // memset(&client_addr, 0, sizeof(client_addr));
+                socklen_t client_addr_len = sizeof(client_addr);
+                int cfd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+                // char client_ip[16];
+                // char *temp = inet_ntoa(client_addr.sin_addr);
+                // strcpy(client_ip, temp);
+                // printf("IP为%s, 端口号为%d的客户端已连接\n", client_ip, ntohs(client_addr.sin_port));
+
+                int flag = fcntl(cfd, F_GETFL);
+                flag |= O_NONBLOCK;
+                fcntl(cfd, F_SETFL, flag);
+
+                event.events = EPOLLIN | EPOLLET;
+                event.data.fd = cfd;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &event);
+            }
+            else {
+                if (events[i].events & EPOLLOUT) {
+                    continue;
+                }
+
+                char recvbuf[10] = {0};
+                int len = 0;
+                while ((len = read(curfd, recvbuf, sizeof(recvbuf))) > 0) {
+                    write(STDOUT_FILENO, recvbuf, strlen(recvbuf));
+                    memset(recvbuf, 0, sizeof(recvbuf));
+                }
+                char *done = "done";
+                write(curfd, done, strlen(done));
+                if (len == -1) {
+                    if (errno == EAGAIN) {
                     }
-                    printf("new connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-                    int flag = fcntl(connfd, F_GETFL);
-                    flag |= O_NONBLOCK;
-                    fcntl(connfd, F_SETFL, flag);
-                    event.data.fd = connfd;
-                    event.events = EPOLLIN | EPOLLET;
-                    epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &event);
-                } else {    // 处理事件
-                    if (events[i].events & EPOLLOUT) {
-                        continue;
-                    }
-                    char recvbuf[5] = {0};
-                    while (ret = read(curfd, recvbuf, 5) > 0) {
-                        char test_buf[6] = {0};
-                        strcpy(test_buf, recvbuf);
-                        printf("recvbuf: %s\n", test_buf);
-                        write(curfd, test_buf, strlen(test_buf));
-                        memset(recvbuf, 0, sizeof(recvbuf));
-                    }
-                    if (ret == -1) {
+                    else {
                         perror("read");
-                        exit(EXIT_FAILURE);
-                    } else {    // 断开连接
-                        printf("client closed...\n");
-                        close(curfd);
-                        epoll_ctl(epfd, EPOLL_CTL_DEL, curfd, NULL);
-                        break;
+                        exit(-1);
                     }
+                }
+                else if (len == 0) {
+                    printf("client close\n");
                 }
             }
         }
-
     }
-    close(listenfd);
+
+    close(listen_fd);
     close(epfd);
     return 0;
 }
